7efb8866f810ef19a17bee5aab9f8c9f952a73c6
Victor Vayuta <eoll073@gmail.com>
2022-06-24 00:39:17 +0300

    всё ?

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,12 +1,12 @@
 package homework
 
-func average(input []float64) float64 {
+func average(input [15]float32) (result float32) {
 	if len(input) == 0 {
-		return float64(0)
+		return float32(0)
 	}
-	sum := 0.0
+	sum := float32(0)
 	for _, v := range input {
 		sum += v
 	}
-	return sum / float64(len(input))
+	return sum / float32(len(input))
 }
