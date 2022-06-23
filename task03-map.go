b04ecd80f7c2df2f849067f83f6fb3046f4bf9c2
Victor <50991855+eoll73@users.noreply.github.com>
2022-06-23 03:22:54 +0300

    firstee commit

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,25 @@
-package homework
+package main
 
-func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+import (
+	"fmt"
+	"sort"
+)
+
+func sortMapValues(input *map[int]string) []string {
+	var output []string
+	var keys []int
+	for k := range *input {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		output = append(output, (*input)[k])
+	}
+	return output
+}
+
+func main() {
+	sortMapValuesInput := map[int]string{2: "a", 0: "b", 1: "c"}
+	sortMapValuesRes := sortMapValues(&sortMapValuesInput)
+	fmt.Printf("sortMapValues result: %v\n", sortMapValuesRes)
 }
